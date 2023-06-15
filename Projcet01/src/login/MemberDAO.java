8dc374210d00c997d69c1ff50d79a162d0d6deba
Class01 <Class01@DESKTOP-UR65MFL>
2023-06-15 21:17:00 +0900

    06.15

---
+++ Projcet01/src/login/MemberDAO.java
@@ -0,0 +1,33 @@
+package login;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class MemberDAO {
+	String driver = "oracle.jdbc.driver.OracleDriver";
+	String url = "jdbc:oracle:thin:@localhost:1521/xe";
+	String user = "c##firstproj";
+	String password = "firstproj";
+
+	private Connection con;
+	private Statement stmt;
+	private ResultSet rs;
+
+	public void connDB() {
+		try {
+			Class.forName(driver);
+			System.out.println("jdbc driver loading success.");
+			con = DriverManager.getConnection(url, user, password);
+			System.out.println("oracle connection success.");
+
+			stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			System.out.println("statement create success.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
--- Projcet01/src/login/login.java
+++ Projcet01/src/login/login.java
@@ -2,33 +2,41 @@ package login;
 
 import java.awt.Button;
 import java.awt.Frame;
-import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.Label;
+import java.awt.TextArea;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
 
 public class login extends WindowAdapter implements ActionListener { 	
+
+//public class login extends JFrame implements ActionListener {	
 	
-	private Frame t, tMain;
-	private TextField tfId, tfPwd, tfMsg;
-	private Button bLogin, bSignup;
-	private Image background=new ImageIcon(login.class.getResource("../image/70_1.png")).getImage();
+	private Frame t, tMain, tsup, tMain2, tMain3;
+	private TextField tfId, tfPwd, tfMsg, tspfid, tspw;
+	private TextArea text;
+	private Button bLogin, bSignup, bsiup, bsech;
+	private JComboBox tlo1, tlo2, tlo21, tlo22, tlo3, tlo4;
+	
+    String[] City={"서울특별시", "부산광역시", "대구광역시", "인천광역시", "광주광역시", "대전광역시", "울산광역시", "경기도", "강원특별자치도", "충청북도", "충청남도", "전라북도", "전라남도", "경상북도", "경상남도", "제주도", "세종특별자치시"};
+    String[] Town={" ", " ", " ", " "};
+    String[] Town1={"강남구", "jhon", "hyosoo", "namyun"};
+    String[] Town2={"사상구", "jhon", "hyosoo", "namyun"};
+    String[] Village={"kim", "jhon", "hyosoo", "namyun"};
+    String[] ETC={"kim", "jhon", "hyosoo", "namyun"};
 	
 	
 	public login() {
-		
-
-		
+			
 		t = new Frame("범죄자 정보 열람 서비스 Crime(로그인)");
 		t.setSize(600, 600);
-		t.setLayout(null);
+		t.setLayout(null); // 프레임 안에서 라벨의 x,y 좌표를 자유롭게 해주는 명령어
 		t.addWindowListener(this);
+
 
 		Label lid = new Label("ID : ");
 		lid.setBounds(50, 190, 50, 50);
@@ -65,13 +73,10 @@ public class login extends WindowAdapter implements ActionListener {
 		t.add(tfPwd);
 		t.add(lid);
 		t.add(tfId);
+		
 
 		t.setVisible(true);
 
-	}
-	
-	public void paint(Graphics g) {
-		g.drawImage(background, 0, 0, null);
 	}
 	
 	public static void main(String[] args) {
@@ -85,7 +90,13 @@ public class login extends WindowAdapter implements ActionListener {
 			tMain.dispose();
 		} else if (e.getComponent() == t){
 			t.dispose();
-		}
+		} else if (e.getComponent() == tsup) {
+			tsup.dispose();
+		} else if (e.getComponent() == tMain2) {
+			tMain2.dispose();
+		} else if (e.getComponent() == tMain3) {
+			tMain3.dispose();
+		} 
 	}
 	
 
@@ -93,19 +104,118 @@ public class login extends WindowAdapter implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource()==bLogin) {
 		// TODO Auto-generated method stub
-		tMain = new Frame("지역찾기");
-		tMain.setBounds(100, 100, 1000, 1000);
-		tMain.addWindowListener(this);
-		tMain.setVisible(true);
-		
-		} else if(e.getSource()==bSignup) {
-			
-			tMain = new Frame("회원가입");
-			tMain.setBounds(300, 300, 800, 800);
-			tMain.addWindowListener(this);
-			tMain.setVisible(true);
-			
-		}
+		tMain2 = new Frame("범죄자 정보 열람 서비스 Crime(지역찾기)");
+		tMain2.setLayout(null);
+		tMain2.setBounds(80, 80, 700, 800);
+		tMain2.addWindowListener(this);
+		tMain2.setVisible(true);
 
+		text = new TextArea();
+		text.setBounds(250, 100, 300, 150);
+		text.setText
+		("지역 세부 정보를 입력하시면\n\n"
+				+ "1.해당 지역의 성범죄자의 신상정보\n\n"
+				+ "2.해당 지역의 강력범죄 발생 빈도\n\n"
+				+ ""
+				+ "를 조회하실 수 있습니다.");
+		text.setEditable(false);
+		text.setFocusable(false);
+		
+		
+		Label tlow1 = new Label("City : ");
+		tlow1.setBounds(130, 270, 100, 100);
+
+		tlo1 = new JComboBox(City);
+		tlo1.setBounds(250, 300, 300, 30);
+		
+		Label tlow2 = new Label("Town : ");
+		tlow2.setBounds(130, 370, 100, 100);
+		
+		tlo2 =new JComboBox(Town);
+		tlo2.setBounds(250, 400, 300, 30);
+		
+		
+//		if (City.equals("서울특별시")) {
+//			
+//			tlo21 = new JComboBox(Town1);
+//			tlo21.setBounds(250, 400, 300, 30);
+//			
+//			} else if (City.equals("부산광역시")) {
+//				
+//				tlo22 = new JComboBox(Town2);
+//				tlo22.setBounds(250, 400, 300, 30);
+//				
+//			}
+		
+		Label tlow3 = new Label("Village : ");
+		tlow3.setBounds(130, 470, 100, 100);
+
+		tlo3 = new JComboBox(Village);
+		tlo3.setBounds(250, 500, 300, 30);
+		
+		Label tlow4 = new Label("ETC : ");
+		tlow4.setBounds(130, 570, 100, 100);
+
+		tlo4 = new JComboBox(ETC);
+		tlo4.setBounds(250, 600, 300, 30);
+		
+		bsech = new Button("Search");
+		bsech.setBounds(350, 670, 80, 30);
+		bsech.addActionListener(this);
+		
+		tMain2.add(tlo1);
+		tMain2.add(tlow1);
+		
+		tMain2.add(tlo2);
+		tMain2.add(tlow2);
+		
+		tMain2.add(tlo3);
+		tMain2.add(tlow3);
+		
+		tMain2.add(tlo4);
+		tMain2.add(tlow4);
+		
+		tMain2.add(bsech);
+		tMain2.add(text); 
+		
+		
+	} else if(e.getSource()==bSignup) {
+			
+			tsup = new Frame("범죄자 정보 열람 서비스 Crime(회원가입)");
+			tsup.setLayout(null);
+			tsup.setBounds(300, 300, 800, 800);
+			tsup.addWindowListener(this);
+			tsup.setVisible(true);
+
+			
+			Label lid2 = new Label("SignUp ID : ");
+			lid2.setBounds(180, 220, 100, 100);
+
+			tspfid = new TextField();
+			tspfid.setBounds(300, 250, 300, 30);
+			
+			Label lid3 = new Label("SignUp PW : ");
+			lid3.setBounds(180, 320, 100, 100);
+
+			tspw = new TextField();
+			tspw.setBounds(300, 350, 300, 30);
+			
+			bsiup = new Button("Sign up");
+			bsiup.setBounds(380, 420, 80, 30);
+			bsiup.addActionListener(this);
+			
+			tsup.add(tspfid);
+			tsup.add(tspw);
+			tsup.add(lid2);
+			tsup.add(lid3);
+			tsup.add(bsiup);
+			
+		} else if (e.getSource()==bsech) {
+			tMain3 = new Frame("범죄자 정보 열람 서비스 Crime(검색결과)");
+			tMain3.setLayout(null);
+			tMain3.setBounds(80, 80, 1000, 1000);
+			tMain3.addWindowListener(this);
+			tMain3.setVisible(true);
+		}
 	}
 }
