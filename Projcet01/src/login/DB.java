2a16e7e46bad3f3a1f1ec004c684b6b34f5ab766
Class01 <Class01@DESKTOP-UR65MFL>
2023-06-22 18:21:54 +0900

    06.22

---
+++ Projcet01/src/login/DB.java
@@ -0,0 +1,75 @@
+package login;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class DB {
+	
+	String url = "jdbc:oracle:thin:@localhost:1521/XE";
+	String user = "c##firstproj";
+	String password = "firstproj";
+	
+	private Connection con;
+	private Statement stmt;
+	private ResultSet rs;
+	
+	public ArrayList<MemberVo> list(String local, String local2 ) {
+		ArrayList<MemberVo> list = new ArrayList<MemberVo>();
+		
+
+		try {
+			connDB();
+
+			String query = "SELECT murder, robbery, theft, violence FROM DB ";
+			if (local != null) {
+				query += "where city ='" + local  + "'";
+				query += " and village ='" + local2  + "'";
+
+			}
+			System.out.println("SQL : " + query);
+
+			rs = stmt.executeQuery(query);
+			rs.last();
+			System.out.println("rs.getRow() : " + rs.getRow());
+
+			if (rs.getRow() == 0) {
+				System.out.println("0 row selected.....");
+				
+			} else {
+				System.out.println(rs.getRow() + "rows selected.......");
+				rs.previous();
+				while (rs.next()) {
+					String murder = rs.getString("murder");
+					String robbery = rs.getString("robbery");
+					String theft = rs.getString("theft");
+					String violence = rs.getString("violence");
+
+					MemberVo data = new MemberVo(murder, robbery, theft, violence);
+					list.add(data);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+	
+	public void connDB() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("jdbc driver loading success.");
+			con = DriverManager.getConnection(url, user, password);
+			System.out.println("oracle connection success.");
+
+			stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			System.out.println("statement create success.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
